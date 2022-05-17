83df26849ff5486781b9e2039e1018ea9ead1d28
Shaman <infografika@inbox.ru>
2022-05-17 17:44:28 +0700

    change task 3, task4

--- dz_8/task_8_4.py
+++ dz_8/task_8_4.py
@@ -14,5 +14,31 @@
     # ValueError: wrong val -5
 # Примечание: сможете ли вы замаскировать работу декоратора?
 
-def val_cheker():
-    pass
+
+
+
+
+def val_cheker(x=0):
+    def cheker_value(func):
+        
+        def wrapper(*args):           
+            if x > 0:
+                msg = func(*args)       
+            if x < 0:
+                msg = print('wrong val {x}')
+                raise ValueError(msg)
+            
+            return msg
+
+        return wrapper
+    return cheker_value
+
+
+
+@val_cheker(lambda x: x>0)
+def calc_cube(num):
+    return num**3
+
+
+a = calc_cube(5)
+print(a)
