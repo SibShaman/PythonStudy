e79491bf1226291f5ee692ce88f008769ba5a176
Shaman <infografika@inbox.ru>
2022-08-13 17:12:50 +0700

    final task_1

--- task_1.py
+++ task_1.py
@@ -4,13 +4,38 @@
 # - 7 -> да
 # - 1 -> нет
 
-your_day = int(input('Введите номер дня недели'))
 
-if  your_day > 7:
-    print('Нет такого дня недели') 
-elif your_day != 6 and your_day != 7:
-    print('Нет - это рабочий день')     
-else:
-    print('Да - это выходной день')
+def first_way():
+    your_day = int(input('Введите номер дня недели'))
+
+    if  your_day > 7:
+        print('Нет такого дня недели') 
+    elif your_day != 6 and your_day != 7:
+        print('Нет - это рабочий день')     
+    else:
+        print('Да - это выходной день')
 
 
+def second_way():
+    my_week = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+    }
+
+    your_day = int(input('Введите номер дня недели'))
+    
+    if  your_day > 7:
+        return 'Нет такого дня недели'
+    elif your_day != 6 and your_day != 7:
+        return f'Нет {my_week[your_day]} - это рабочий день'
+    else:
+        return f'Да {my_week[your_day]} - это выходной день'
+
+
+print(first_way())
+print(second_way())
