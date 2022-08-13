1f50bb2b7be475f1b812b0d58469ab2bd0bd48ba
Shaman <infografika@inbox.ru>
2022-08-13 13:53:18 +0700

    tsak_1 - done

--- task_1.py
+++ task_1.py
@@ -4,4 +4,13 @@
 # - 7 -> да
 # - 1 -> нет
 
+your_day = int(input('Введите номер дня недели'))
+
+if  your_day > 7:
+    print('Нет такого дня недели') 
+elif your_day != 6 and your_day != 7:
+    print('Нет - это рабочий день')     
+else:
+    print('Да - это выходной день')
+
 
