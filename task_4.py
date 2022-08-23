c4af57896c971cb1e2626de52616a942648797ac
Shaman <infografika@inbox.ru>
2022-08-23 22:58:17 +0700

    final task4

--- task_4.py
+++ task_4.py
@@ -4,3 +4,14 @@
 - 3 -> 11
 - 2 -> 10
 """
+
+your_num = int(input('Введите число которое необходимо перевести в двоичную систему'))
+
+res_list = []
+while your_num > 0:
+    a = your_num%2
+    b = your_num//2
+    your_num = b
+    res_list.append(a)
+
+print(*reversed(res_list))
