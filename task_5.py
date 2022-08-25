f8bef33bcb58953fd3e5b1cd8f67dc782a22c692
Shaman <infografika@inbox.ru>
2022-08-25 20:59:23 +0700

    open files

--- task_5.py
+++ task_5.py
@@ -1,2 +1,6 @@
 # Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('file_one', 'a', encoding='utf-8') as first, \
+    open('file_two', 'a', encoding='utf-8') as second:
+    pass
