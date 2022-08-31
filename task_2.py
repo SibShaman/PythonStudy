be718437535b6f88525e86ef153169013dff9fda
Shaman <infografika@inbox.ru>
2022-09-01 00:12:47 +0700

    sketch task_2

--- task_2.py
+++ task_2.py
@@ -4,8 +4,8 @@
 # состоящий из элементов последовательности 3n + 1.
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-N = int(input('Введите количество чисел в последовательности: '))
+# N = int(input('Введите количество чисел в последовательности: '))
 
 
-my_dict = dict(lambda x: (x*i + 1) for i in range(1, 6))
-print(*my_dict)
+# my_dict = dict(lambda x: (x*i + 1) for i in range(1, 6))
+# print(*my_dict)
