10fa10c0a439322f52f9fc294253d33325e1f905
Shaman <infografika@inbox.ru>
2022-09-01 15:51:22 +0700

    final task_3

--- task_3.py
+++ task_3.py
@@ -1,3 +1,7 @@
 """Урок 6. Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension. """
 
 # 14. Подсчитать сумму цифр в вещественном числе.
+
+num = input('Введите число которое вы хотите разложить и посчитать сумму: ')
+temp_list = [int(i) for x, i in enumerate(num)]
+print(f'сумма цифр в вашем числе = {sum(temp_list)}')
