6bef4abb0ea2c5a6c2d7687b40e8821ebde7505c
Shaman <infografika@inbox.ru>
2022-09-01 18:25:14 +0700

    final task_5

--- task_5.py
+++ task_5.py
@@ -1,3 +1,10 @@
 """Урок 6. Ускоренная обработка данных: lambda, filter, map, zip, enumerate, list comprehension. """
 
 # 16. Задать список из n чисел последовательности и вывести на экран их сумму.
+
+from functools import reduce
+
+
+num = int(input('Введите количество чисел в последовательности: '))
+sum_num = reduce(lambda x, y: x+y, range(1, num+1))
+print(f'Сумма чисел в последовательности = {sum_num}')
