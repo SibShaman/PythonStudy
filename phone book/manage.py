2eba52b4ddacb87f8a284af7bfdb7eb7c8e4f29c
Shaman <infografika@inbox.ru>
2022-09-06 00:06:22 +0700

    change dz

--- phone book/manage.py
+++ phone book/manage.py
@@ -1,2 +1,19 @@
 """ modul for send data in files,
         get data from files"""
+
+import user_interface as ui
+import views
+
+
+def send_data():
+    ui.add_contact()
+    with open('phone_book.txt', 'a', encoding='utf8') as file:
+        file.write(f"{' '.join(ui.contact_list)}\n")
+
+
+def get_data():
+    pass
+
+
+# print(send_data())
+send_data()
--- phone book/user_interface.py
+++ phone book/user_interface.py
@@ -2,9 +2,20 @@
 # методы  для ввода и изменения информации в справочнике
 
 
+contact_list = []
+
+
 def add_contact():
     """ Добавление в тел.книгу"""
-    pass
+    first_name = input('Введите имя: ')
+    second_name = input('Введите фамилию: ')
+    phone = input('Введите номер телефона: ')
+    descriptor = input('Введите описание: ')
+    contact_list.append(first_name)
+    contact_list.append(second_name)
+    contact_list.append(phone)
+    contact_list.append(descriptor)
+    return contact_list
 
 
 def find_contact():
