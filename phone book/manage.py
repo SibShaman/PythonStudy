b1b39ebe03996e264ceae9c3dc6fd4eefceaa35d
Shaman <infografika@inbox.ru>
2022-09-09 12:09:05 +0700

    add read_data in manage

--- phone book/manage.py
+++ phone book/manage.py
@@ -2,38 +2,42 @@
         read data from files"""
 import os
 import csv
-import user_interface as ui
+# import user_interface as ui
 # import views
 
 
-file_name = 'phone_book.csv'
-file_exists = os.path.isfile(file_name)
+FILE_NAME = 'phone_book.csv'
+file_exists = os.path.isfile(FILE_NAME)
 
 
-def write_data():
+def write_data(list_: list, file: str):
     """ Пишем в файл csv"""
-    with open(file_name, 'a', encoding='utf8', newline='') as write_file:
+    with open(file, 'a', encoding='utf8', newline='') as write_file:
         fieldnames = ['first_name', 'second_name', 'phone', 'descriptor']
         writer = csv.DictWriter(
             write_file, fieldnames=fieldnames, delimiter=' ')
         if not file_exists:
             writer.writeheader()
-        for line in ui.contact_list:
+        for line in list_:
             writer.writerow(line)
 
 
-# ui.add_contact()
-# write_data()
-# ui.find_contact()
+def read_data(file: str) -> list:
+    """Чтение файла и с возвращением списка списков из каждой строки """
+    with open(file, 'r', encoding='utf8') as read_file:
+        reader = csv.reader(read_file, delimiter=' ')
+        temp_list = []
+        for row in reader:
+            temp_list.append(row)
+        return temp_list
 
 
-def read_data():
-    """Читаем из файла csv"""
-    with open(file_name, 'r', encoding='utf8') as read_file:
-        dataset = csv.reader(read_file, delimiter=' ')
-        for row in dataset:
+def show_data(file: str) -> str:
+    """Читаем из файла csv и выводим в консоль все """
+    with open(file, 'r', encoding='utf8') as read_file:
+        reader = csv.reader(read_file, delimiter=' ')
+        for row in reader:
             print(', '.join(row))
 
 
-read_data()
-print(ui.update_contact())
+# print(read_data(FILE_NAME))
