c32d13288d68e9e1be8eca3af5c1c63da7533851
Shaman <infografika@inbox.ru>
2022-09-14 09:53:50 +0700

    fix

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 """ Phone Book"""
 # точка входа в приложение
-from phone_book import add_contact_in_book, changed_contact, remove_contact
-
-
-add_contact_in_book()
-# add_contact_in_book()
-remove_contact()
-changed_contact()
+from phone_book import add_contact_in_book, changed_contact, remove_contact, search_contact
