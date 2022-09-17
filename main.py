865405aee823a12f000f8b6eddb08b7f5f3777f0
Shaman <infografika@inbox.ru>
2022-09-17 12:14:41 +0700

    menu bot test

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pathlib
 import logging
 from dotenv import load_dotenv, dotenv_values
 from aiogram import Bot, Dispatcher, executor, types
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.callback_data import CallbackData
+from phone_book import add_contact_in_book
 
 BASE_DIR = pathlib.Path(__file__).parent
 ENV_FILE_PATH = BASE_DIR / '.env'
@@ -18,23 +20,27 @@ TOKEN = os.environ.get('TOKEN')
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+my_command = CallbackData('function')
 
 
 @dp.message_handler(commands='start')
-async def start_cmd_handler(message: types.Message):
-
+async def start_handler(message: types.Message):
+    """ Запуск бота с инлайн клавиатурой и выбором действия"""
     row_btns = [
         types.InlineKeyboardButton(
-            text='Добавить контакт', callback_data='add_contact'),
+            text='Добавить контакт', callback_data=my_command.new(function=add_contact_in_book())),
         types.InlineKeyboardButton(
-            text='Найти контакт', callback_data='find_contact'),
+            text='Найти контакт', callback_data=my_command.new(function='find_contact')),
         types.InlineKeyboardButton(
-            text='Изменить контакт', callback_data='changed_contact'),
+            text='Изменить контакт', callback_data=my_command.new(function='changed_contact')),
         types.InlineKeyboardButton(
-            text='Удалить контакт', callback_data='del_contact'),
+            text='Удалить контакт', callback_data=my_command.new(function='del_contact')),
         types.InlineKeyboardButton(
-            text='Показать всю книгу', callback_data='show_contact'),
+            text='Показать всю книгу', callback_data=my_command.new(function='show_contact')),
     ]
     keyboard_markup = types.InlineKeyboardMarkup(row_width=2)
     keyboard_markup.add(*row_btns)
@@ -42,22 +48,10 @@ async def start_cmd_handler(message: types.Message):
     await message.answer("PHONE BOOK", reply_markup=keyboard_markup)
 
 
-# Use multiple registrators. Handler will execute when one of the filters is OK
-# @dp.callback_query_handler(text='no')  # if cb.data == 'no'
-# @dp.callback_query_handler(text='yes')  # if cb.data == 'yes'
-# async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-#     answer_data = query.data
-#     # always answer callback queries, even if you have nothing to say
-#     await query.answer(f'You answered with {answer_data!r}')
-
-#     if answer_data == 'yes':
-#         text = 'Great, me too!'
-#     elif answer_data == 'no':
-#         text = 'Oh no...Why so?'
-#     else:
-#         text = f'Unexpected callback data {answer_data!r}!'
-
-#     await bot.send_message(query.from_user.id, text)
+# @dp.callback_query_handler(my_command.filter())
+# async def button_press(call: types.CallbackQuery, callback_data: dict):
+#     if callback_data.get('add_contact'):
+#         print('hello')
 
 
 if __name__ == '__main__':
